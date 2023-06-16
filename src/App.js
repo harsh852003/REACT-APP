f697561081712b2ec13f92292502e879206374c9
Bhaskar Pandya <pandyaharsh852003@gmail.com>
2023-06-16 16:04:10 +0530

    added dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React,{ useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-{/*import About from './components/about';*/}
+import About from './components/about';
 function App() {  
+  const [mode,setMode] = useState('light') // it shows the dark mode is enable or not.
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'darkgreen'
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+
+    }
+  }
   return (
   <>
-  <Navbar title = 'TextUtils' aboutText="About"/>
+  <Navbar title = 'TextUtils' aboutText="About" mode = {mode} toggleMode = {toggleMode}/>
   <div className="container my-3">
 
-  <TextForm heading = ""/>
+  <TextForm heading = "Enter the Text for Analyse" mode={mode}/>
 
   {/*<About/>*/}
   </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,27 +18,33 @@ export default function TextForm(props) {
     const handleClearClick = () =>{
         setText("")
     }
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+    }
     
     
     return (
         <>
-            <div className="container">
+            <div className="container" style ={{color: props.mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="TextArea">
                 <label htmlFor="exampleFormControlTextarea1" className="form-label" >{props.text}</label>
-                <textarea className="form-control" value={text} onChange={handleChange}  id="exampleFormControlTextarea1" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleChange} style = {{backgroundColor:props.mode === 'dark'?'git areen':'white',color: props.mode === 'dark'?'white':'black' }}  id="exampleFormControlTextarea1" rows="8"></textarea>
             </div>
             <button className="btn btn-success my-3" onClick={handleClick}>Convert to UpperCase</button>
             <button className="btn btn-success mx-3" onClick={handleOnClick}>Convert to LowerCase</button>
             <button className="btn btn-success mx-0" onClick={handleClearClick}>Clear the text</button>
+            <button className="btn btn-success mx-3" onClick={handleExtraSpaces}>Remove Extra Spacet</button>
+
             </div>
-            <div className="container my-3">
+            <div className= "container my-3" style = {{color: props.mode === 'dark'?'white':'black'}}>
                 <h2>Your Text Summary</h2>
                 <p>no of words - {text.split(" ").length}</p>
                 <p>no of character- {text.length}</p>
                 <p>{0.008 * text.split(" ").length} per min read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0 ? text : "Plaese rite something to preview"}</p>
             </div>
 
 
