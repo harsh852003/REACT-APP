e8e7247b045bf015b1af01f09d031c273f66babb
HARSH PANDYA <117256499+harsh852003@users.noreply.github.com>
2023-06-23 21:58:47 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,7 +8,8 @@ export default function TextForm(props) {
         props.showAlert("Converted into Uppercase","success")
     }
     const handleChange = (event) => {
-        setText(event.target.value)  
+        setText(event.target.value)
+       
     }
     const handleOnClick = () => {
         let newText = text.toLowerCase()
@@ -24,6 +25,7 @@ export default function TextForm(props) {
         setText(newText.join(" "))
         props.showAlert("Extra Space Removed", "success")
     }
+
     return (
         <>
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
@@ -32,18 +34,18 @@ export default function TextForm(props) {
                     <label htmlFor="exampleFormControlTextarea1" className="form-label" >{props.text}</label>
                     <textarea className="form-control" value={text} onChange={handleChange} style={{ backgroundColor: props.mode === 'dark' ? 'off white' : 'white', color: props.mode === 'dark' ? 'green' : 'black' }} id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
-                <button className="btn btn-success my-3" onClick={handleClick}>Convert to UpperCase</button>
-                <button className="btn btn-success mx-3" onClick={handleOnClick}>Convert to LowerCase</button>
-                <button className="btn btn-success mx-0" onClick={handleClearClick}>Clear the text</button>
-                <button className="btn btn-success mx-3" onClick={handleExtraSpaces}>Remove Extra Spacet</button>
+                <button className="btn btn-success my-1 my-2" onClick={handleClick}>Convert to UpperCase</button>
+                <button className="btn btn-success mx-1 my-2" onClick={handleOnClick}>Convert to LowerCase</button>
+                <button className="btn btn-success mx-1 my-2" onClick={handleClearClick}>Clear the text</button>
+                <button className="btn btn-success mx-1 my-2" onClick={handleExtraSpaces}>Remove Extra Spacet</button>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h2>Text Summary</h2>
-                <p>no of words - {text.split(" ").length}</p>
+                <h2>Your Text Summary</h2>
+                <p>no of words - { text.split(" ").filter((element)=>{return element.length !=0}).length}</p>
                 <p>no of character- {text.length}</p>
                 <p>{0.008 * text.split(" ").length} per min read</p>
                 <h2>Preview</h2>
-                <p>{text.length > 0 ? text : "Plaese write something to preview"}</p>
+                <p>{text.length > 0 ? text : "Plaese rite something to preview"}</p>
             </div>
         </>
     )
